ae38106768fd737905ebe567adad8027e6455f4c
Sergio <sergiohomet@gmail.com>
2024-12-16 23:45:35 -0300

    Agregando .ts con los datos de la carta

---
+++ src/data/carta.ts
@@ -0,0 +1,8 @@
+export const carta = [
+    {id: 1, 'name': 'Comun', price: '6000'},
+    {id: 2, 'name': 'Jamon y Morrones', price: '7000'},
+    {id: 3, 'name': 'Calabresa', price: '7000'},
+    {id: 4, 'name': 'Napolitana', price: '7000'},
+    {id: 5, 'name': 'Sandwich de pollo', price: '5000'},
+    {id: 6, 'name': 'Sandwich de Matambre', price: '5000'},
+]
